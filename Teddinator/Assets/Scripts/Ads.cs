11f4df51e8500c7454f38d5803886c26300bc072
mauricio11 <mauriciodiaz.1989@yahoo.com>
2015-11-10 21:23:01 -0800

    Fixed Ads script to work with play button
    Saved scene and project, fixed some text sizing

--- Teddinator/Assets/Scripts/Ads.cs
+++ Teddinator/Assets/Scripts/Ads.cs
@@ -5,13 +5,6 @@ using UnityEngine.Advertisements;
 
 public class Ads : MBSingleton<Ads> {
 
-	// Use this for initialization
-	void Start () 
-	{
-		Advertisement.Initialize ("1010284", true);
-	}
-	
-
 	public void PlayAd () 
 	{
 		Advertisement.Show ();
