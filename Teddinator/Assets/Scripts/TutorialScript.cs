2068681606794d111b830082a66dde06963bec7d
mauricio11 <mauriciodiaz.1989@yahoo.com>
2016-02-21 23:37:01 -0800

    Created TutorialPanelClosing_Animation

---
+++ Teddinator/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialScript : MonoBehaviour {
+
+	public Animator panelClosingAnim;
+
+	public void CloseTutorialPanel()
+	{
+		//panelClosingAnim.Play ();
+	}
+}
