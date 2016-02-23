134c0dc7a5b96e28ce164f03e69cdcc652220a9d
mauricio11 <mauriciodiaz.1989@yahoo.com>
2016-02-22 22:44:10 -0800

    Part 1 of tutorial in now completed

--- Teddinator/Assets/Scripts/TutorialScript.cs
+++ Teddinator/Assets/Scripts/TutorialScript.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class TutorialScript : MonoBehaviour {
 
 	public Animator panelClosingAnim;
-	public Animator TeddyAnim;
+	public Animator teddyAnim;
+	public GameObject part1Panel;
+	public GameObject joyStick;
+
+
 
 	public void CloseTutorialPanel()
 	{
 		//calls mecanim transition to close panel
 		panelClosingAnim.SetBool("ClosePanel",true);
-		StartCoroutine ("TeddyFlyIn", 3);
+		StartCoroutine ("TeddyFlyIn", 2);
 	}
 
 	public void Quit()
@@ -29,6 +34,19 @@ public class TutorialScript : MonoBehaviour {
 	IEnumerator TeddyFlyIn(float second)
 	{
 		yield return new WaitForSeconds (second);
-		TeddyAnim.SetBool ("FlyIn", true);
+		teddyAnim.SetBool ("FlyIn", true);
+
+		yield return new WaitForSeconds (second);
+		part1Panel.SetActive (true);
+		joyStick.SetActive (true);
+
+		//turns off PlayerControl script to prevent shooting
+		GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerControl> ().enabled = true;
+
+		yield return new WaitForSeconds (second);//Prevents shield bug to turn on
+
+		//turns on shield because it is defaulted off.
+		GameObject.FindGameObjectWithTag ("Player").transform.FindChild ("ShockwavePlayer").gameObject.SetActive (true);
+		teddyAnim.enabled = false;
 	}
 }
