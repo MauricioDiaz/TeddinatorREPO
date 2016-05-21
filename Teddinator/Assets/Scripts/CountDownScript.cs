f5df1f155386e520d87d3b96e4107cd9bb23da86
mauricio11 <mauriciodiaz.1989@yahoo.com>
2016-05-21 15:21:32 -0700

    Countdown working
    Need to pause everything till GO!

---
+++ Teddinator/Assets/Scripts/CountDownScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CountDownScript : MonoBehaviour {
+
+	public Text one;
+	public Text two;
+	public Text three;
+	public Text go;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (StartCountDown (1));	
+	}
+	
+	IEnumerator StartCountDown(float sec)
+	{
+		yield return new WaitForSeconds (sec);
+		one.enabled = true;
+		yield return new WaitForSeconds (sec);
+		one.enabled = false;
+		two.enabled = true;
+		yield return new WaitForSeconds (sec);
+		two.enabled = false;
+		three.enabled = true;
+		yield return new WaitForSeconds (sec);
+		three.enabled = false;
+		go.enabled = true;
+		yield return new WaitForSeconds (sec);
+		go.enabled = false;
+	
+	}
+}
