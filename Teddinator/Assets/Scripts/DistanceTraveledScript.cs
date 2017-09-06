7afaed173da536880471a74567264cbef683b798
mauricio11 <mauriciodiaz.1989@yahoo.com>
2017-09-05 23:21:36 -0700

    Added Distance to GameOverPanel
    Also changed text color.

---
+++ Teddinator/Assets/Scripts/DistanceTraveledScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DistanceTraveledScript : MonoBehaviour {
+
+	public Text distance_text;
+	//public float distance;
+
+	public float timer;
+
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		distance_text.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		timer += Time.deltaTime;
+		distance_text.text = ("Distance: " + timer.ToString("F2"));//Keeps decimal number to the tenths
+		//Debug.Log (timer.ToString ());
+
+	
+	}
+}
